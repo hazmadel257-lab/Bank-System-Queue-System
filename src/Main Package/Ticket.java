4f54c2277ae78dbb93a51631aff0cb586f5bd23c
hazmadel257-lab <hazmadel257@gmail.com>
2025-12-15 02:52:36 +0200

    Update Ticket.java

--- src/Main Package/Ticket.java
+++ src/Main Package/Ticket.java
@@ -1,4 +1,30 @@
 public class Ticket {
 
+    private int ticketNo;
+    private int priority;
+    private Customer owner;
 
+    public int getTicketNo() {
+        return ticketNo;
+    }
+
+    public void setTicketNo(int ticketNo) {
+        this.ticketNo = ticketNo;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+
+    public Customer getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Customer owner) {
+        this.owner = owner;
+    }
 }
